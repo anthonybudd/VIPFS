bc18f9ac743f5df70f8ca11d28a17238c5bd522b
Anthony Budd <anthonybudd94@gmail.com>
2020-01-06 16:31:39 -0600

    MIT license Gang

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
+const express = require('express')
 
-var app = express();
-app.use(express.static('./build'));
-app.get('/app.js', (req, res) => res.sendFile(__dirname + '/build/app.js'));
-app.get('/app.css', (req, res) => res.sendFile(__dirname + '/build/app.css'));
-app.get('/*', (req, res) => res.sendFile(__dirname + '/build/index.html'));
+const app = express()
+app.use(express.static('./build'))
+app.get('/app.js', (req, res) => res.sendFile(__dirname + '/build/app.js'))
+app.get('/app.css', (req, res) => res.sendFile(__dirname + '/build/app.css'))
+app.get('/*', (req, res) => res.sendFile(__dirname + '/build/index.html'))
 
-const PORT = process.env.APP_PORT || 8000;
-module.exports = app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+const PORT = process.env.APP_PORT || 8000
+module.exports = app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
